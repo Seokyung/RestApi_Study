050f52340ec52e3f8ec7b580a61f506d5533cbbd
Seokyung <xuejd17@gmail.com>
2023-08-10 18:09:24 +0900

    Implement user signup/login api

--- server/app.js
+++ server/app.js
@@ -1,38 +1,28 @@
-const express = require("express");
+import express from "express";
+import * as argon2 from "argon2";
+import { text_data } from "./text_data.js";
+import { users_data } from "./users_data.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const test_data = [
-	{
-		id: 1,
-		title: "text 1",
-	},
-	{
-		id: 2,
-		title: "text 2",
-	},
-	{
-		id: 3,
-		title: "text 3",
-	},
-];
+/*** 데이터 CRUD ***/
 
 app.get("/", function (req, res) {
 	res.send("Hello World from SK!");
 });
 
-// GET - test_data 전체 조회
-app.get("/test", function (req, res) {
-	res.send(test_data);
+// GET - text_data 전체 조회
+app.get("/text", function (req, res) {
+	res.send(text_data);
 });
 
-// GET - test_data 중 원하는 값 조회
-app.get("/test/:id", function (req, res) {
+// GET - text_data 중 원하는 값 조회
+app.get("/text/:id", function (req, res) {
 	const id = req.params.id;
-	const data = test_data.find((el) => el.id === Number(id));
-
+	const data = text_data.find((el) => el.id === Number(id));
 	if (data) {
 		res.send(data);
 	} else {
@@ -40,35 +30,83 @@ app.get("/test/:id", function (req, res) {
 	}
 });
 
-// POST - test_data 값 추가 (생성)
-app.post("/test", function (req, res) {
+// POST - text_data 값 추가 (생성)
+app.post("/text", function (req, res) {
 	const title = req.body.title;
-
-	test_data.push({
-		id: test_data.length + 1,
-		title,
+	text_data.push({
+		id: text_data.length + 1,
+		text,
 	});
-
 	res.send("POST - 값 추가 완료!");
 });
 
-// PUT - test_data 값 수정
-app.put("/test", function (req, res) {
+// PUT - text_data 값 수정
+app.put("/text", function (req, res) {
 	const id = req.body.id;
 	const title = req.body.title;
-
-	test_data[id - 1].title = title;
-
+	text_data[id - 1].title = title;
 	res.send("PUT - 값 수정 완료!");
 });
 
-// DELETE - test_data 값 삭제
-app.delete("/test", function (req, res) {
+// DELETE - text_data 값 삭제
+app.delete("/text", function (req, res) {
 	const id = req.body.id;
-
-	test_data.splice(id - 1, 1);
-
+	text_data.splice(id - 1, 1);
 	res.send("DELETE - 값 삭제 완료!");
+});
+
+/*** user 인증 ***/
+
+// GET - users 전체 조회
+app.get("/users", (req, res) => {
+	res.send(users_data);
+});
+
+// POST - users에 유저 정보 추가 (signup)
+app.post("/signup", async (req, res) => {
+	const { userId, password, userName, age, email } = req.body;
+	try {
+		const hash = await argon2.hash(password); // argon2로 비밀번호 암호화
+		users_data.push({
+			id: users_data.length + 1,
+			userId,
+			password: hash,
+			userName,
+			age,
+			email,
+		});
+		res.send("POST - signup 완료!");
+	} catch (err) {
+		res.send("Signup Error", err);
+	}
+});
+
+// POST - 유저 로그인 (login)
+app.post("/login", async (req, res) => {
+	const { userId, password } = req.body;
+	const user = users_data.filter((user) => {
+		return user.userId === userId;
+	});
+
+	try {
+		if (user.length === 0) {
+			// id did not match
+			res.status(403).send("해당하는 아이디가 존재하지 않습니다.");
+			return;
+		}
+
+		if (await argon2.verify(user[0].password, password)) {
+			// password match
+			res.send("POST - login 성공!");
+		} else {
+			// password did not match
+			res.status(403).send("잘못된 비밀번호 입니다.");
+			return;
+		}
+	} catch (err) {
+		// internal failure
+		res.send("Login Error: ", err);
+	}
 });
 
 app.listen(3000, () => {
---
+++ server/users_data.js
@@ -0,0 +1,18 @@
+export const users_data = [
+	{
+		id: 1,
+		userId: "java_kim11",
+		password: "jvkm1111",
+		userName: "김자바",
+		age: 25,
+		email: "java11@aa.bb",
+	},
+	{
+		id: 2,
+		userId: "scrrrr2",
+		password: "tpircs2sj",
+		userName: "박스크립트",
+		age: 21,
+		email: "parkjs@aa.bb",
+	},
+];
