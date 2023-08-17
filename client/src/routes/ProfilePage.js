927d44a641f1382349fc2b92a1b731ffd73ede26
Seokyung <xuejd17@gmail.com>
2023-08-18 01:17:45 +0900

    Implement logout

--- client/src/routes/ProfilePage.js
+++ client/src/routes/ProfilePage.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 function ProfilePage() {
+	const onLogout = () => {
+		const isLogout = window.confirm("Are you sure to logout?");
+		if (isLogout) {
+			window.localStorage.clear();
+			window.location.reload();
+		} else {
+			return;
+		}
+	};
+
 	return (
 		<Container fluid className="mt-3 mb-3">
 			<Row>
@@ -13,6 +23,11 @@ function ProfilePage() {
 						</Card.Header>
 						<Card.Body></Card.Body>
 					</Card>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<Button onClick={onLogout}>Logout</Button>
 				</Col>
 			</Row>
 		</Container>
